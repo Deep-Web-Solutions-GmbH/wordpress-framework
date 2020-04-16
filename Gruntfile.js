4e132130e73264b69ea1aea8c3ebd1bfbe973c8a
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2020-04-16 18:53:19 +0200

    Created first draft of a GruntFile and added a POT file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,68 @@
 module.exports = function( grunt ) {
     'use strict';
 
+    // Load all grunt tasks matching the `grunt-*` pattern
+    require( 'load-grunt-tasks' )( grunt );
 
+    // Show elapsed time
+    require( 'time-grunt' )( grunt );
+
+    // Project configuration
+    grunt.initConfig({
+        package: grunt.file.readJSON( 'package.json' ),
+        dirs: {
+            lang: 'src/languages'
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            options: {
+                globals: {
+                    jQuery: true
+                }
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
+        },
+        makepot: {
+            framework: {
+                options: {
+                    cwd: 'src',
+                    domainPath: 'languages',
+                    exclude: [],
+                    potFilename: 'dws-wp-framework.pot',
+                    mainFile: 'index.php',
+                    potHeaders: {
+                        'report-msgid-bugs-to': 'https://github.com/Deep-Web-Solutions-GmbH/wordpress-framework-core/issues',
+                        'project-id-version': '<%= package.title %> <%= package.version %>'
+                    },
+                    processPot: function( pot ) {
+                        delete pot.headers['x-generator'];
+                        return pot;
+                    },
+                    type: 'wp-plugin',
+                    updateTimestamp: false,
+                    updatePoFiles: true
+                }
+            }
+        },
+        potomo: {
+            framework: {
+                options: {
+                    poDel: false
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= dirs.lang %>',
+                    src: ['*.po'],
+                    dest: '<%= dirs.lang %>',
+                    ext: '.mo',
+                    nonull: true
+                }]
+            }
+        }
+    });
+
+    grunt.registerTask( 'default', ['jshint', 'makepot', 'potomo'] );
 }
