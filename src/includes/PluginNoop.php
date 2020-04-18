0be27756eea5b6bdbe02915ee30e2d8f6b821d36
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2020-04-18 17:05:58 +0200

    Temp status

---
+++ src/includes/PluginNoop.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DeepWebSolutions\Framework\Core\v1_0_0;
+
+use DeepWebSolutions\Framework\Core\v1_0_0\Abstracts\PluginBase;
+
+defined( 'ABSPATH' ) || exit;
+
+final class PluginNoop extends PluginBase {
+
+}
