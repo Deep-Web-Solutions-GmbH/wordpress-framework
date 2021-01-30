cfa708f20026fc8a5150374cc4b7aae84cf3fa99
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-01-30 19:54:22 +0100

    improved the dependencies trait

--- src/includes/Traits/Dependencies.php
+++ src/includes/Traits/Dependencies.php
@@ -12,6 +12,23 @@ use DeepWebSolutions\Framework\Utilities\DependenciesChecker;
  * @package DeepWebSolutions\Framework\Core\Traits
  */
 trait Dependencies {
+	// region FIELDS AND CONSTANTS
+
+	/**
+	 * The local instance of a dependencies checker.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @access  protected
+	 * @var     DependenciesChecker
+	 */
+	protected ?DependenciesChecker $dependencies_checker = null;
+
+	// endregion
+
+	// region METHODS
+
 	/**
 	 * Return a dependency checker instance to check activation conditions against.
 	 *
@@ -20,5 +37,25 @@ trait Dependencies {
 	 *
 	 * @return  DependenciesChecker
 	 */
-	abstract public function get_dependencies_checker(): DependenciesChecker;
+	public function get_dependencies_checker(): DependenciesChecker {
+		if ( is_null( $this->dependencies_checker ) ) {
+			$this->dependencies_checker = new DependenciesChecker( $this->get_dependencies() );
+		}
+
+		return $this->dependencies_checker;
+	}
+
+	/**
+	 * The child class needs to return a valid configuration for the DependenciesChecker.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @see     DependenciesChecker::__constructor()
+	 *
+	 * @return  array
+	 */
+	abstract protected function get_dependencies(): array;
+
+	// endregion
 }
